810ae397a43c52208f2cc202d8e267410adcbbae
Matt <mattnappo@gmail.com>
2019-09-05 19:58:13 -0400

    Wrote encryption abstraction.

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/xoreo/flash-encrypt/crypto"
 	"github.com/xoreo/flash-encrypt/fs"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func getDrives() ([]string, error) {
@@ -15,6 +17,7 @@ func getDrives() ([]string, error) {
 		panic(err)
 	}
 
+	fmt.Println("Drives:")
 	for i, drive := range drives {
 		fmt.Printf("[%d] %s\n", i, drive)
 	}
@@ -35,55 +38,71 @@ func encrypt() error {
 	if err != nil {
 		panic(err)
 	}
+	targetDrive = strings.TrimSuffix(targetDrive, "\n")
 
 	for i, drive := range drives {
-		if string(i) == targetDrive {
+		if strconv.Itoa(i) == targetDrive {
 			fmt.Print("Are you sure you want to encrypt " + drive + " (yes/no)? ")
 			confirmation, err := reader.ReadString('\n')
 			if err != nil {
 				return err
 			}
+			confirmation = strings.TrimSuffix(confirmation, "\n")
 
 			if confirmation == "yes" {
-				fmt.Print("passphrase: ")
+				fmt.Print("Passphrase: ")
 				passphrase, err := reader.ReadString('\n')
 				if err != nil {
 					return err
 				}
+				confirmation = strings.TrimSuffix(confirmation, "\n")
+
 				err = crypto.EncryptDir(fs.GetDrivePath(drive), passphrase)
 				if err != nil {
 					return err
 				}
 				fmt.Printf("Encrypted %s\n", drive)
+
+			} else {
+				return nil
 			}
 
 		}
 	}
-
-	fmt.Println(text)
+	return nil
 }
 
-func decrypt() {
+func decrypt() error {
 
+	return nil
 }
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("[0] Encrypt\n[1] Decrypt")
-	whereto, err := reader.ReadString('\n')
+	option, err := reader.ReadString('\n')
 	if err != nil {
 		panic(err)
 	}
+	option = strings.TrimSuffix(option, "\n")
 
-	switch whereto {
+	switch option {
 	case "0":
-		encrypt()
+		err = encrypt()
+		if err != nil {
+			panic(err)
+		}
 		break
+
 	case "1":
-		decrypt()
+		err = decrypt()
+		if err != nil {
+			panic(err)
+		}
+
 		break
 	default:
-		panic(errors.New(fmt.Sprintf("'%s' is not an option", whereto)))
+		panic(errors.New(fmt.Sprintf("'%s' is not an option", option)))
 	}
 
 }
