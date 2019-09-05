ebbaafae15a579223993304a1990c35351bdb294
Matt <mattnappo@gmail.com>
2019-09-04 22:05:42 -0400

    Testing.

--- crypto/aes_test.go
+++ crypto/aes_test.go
@@ -3,9 +3,19 @@ package crypto
 import "testing"
 
 func TestEncryptFile(t *testing.T) {
+	passphrase := "secret passphrase"
 
+	err := EncryptFile("./testfile", passphrase)
+	if err != nil {
+		t.Fatal(err)
+	}
 }
 
 func TestDecryptFile(t *testing.T) {
+	passphrase := "secret passphrase"
 
+	err := DecryptFile("./testfile", passphrase)
+	if err != nil {
+		t.Fatal(err)
+	}
 }
